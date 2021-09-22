2d3ce21062350f6b0f6f8416e1d5b2a6bb1bd403
Anthony Fu <anthonyfu117@hotmail.com>
2021-09-22 11:03:10 +0800

    docs: update docs

--- packages/.vitepress/theme/components/LearnMoreComponents.vue
+++ packages/.vitepress/theme/components/LearnMoreComponents.vue
@@ -1,3 +1,3 @@
 <template>
-  This function also provides a headless component version via the <code>@vueuse/components</code> package. <a href="/guide/components">Learn more about the usage</a>.
+  This function also provides a renderless component version via the <code>@vueuse/components</code> package. <a href="/guide/components">Learn more about the usage</a>.
 </template>
